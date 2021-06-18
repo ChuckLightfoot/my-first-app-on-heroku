3cb0fc1291be9f14ee90ea09c9a8ddd626ce7017
Nolan Hovis <78943909+NolanHovis@users.noreply.github.com>
2021-06-17 19:17:04 -0500

    Changes made by Lee

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-//Install express server
+// Install Express Server
 const express = require('express');
-const path = require('path');
+const path =require('path');
 const app = express();
 
 // Server only are stactic files from the dist directory
 app.use(express.static('./dist/my-first-app-on-heroku'));
+
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/my-first-app-on-heroku'}),
+res.sendFile('index.html', {root: 'dist/my-first-app-on-heroku'}),
 );
 
 // Tell the app how to start on Heroku
